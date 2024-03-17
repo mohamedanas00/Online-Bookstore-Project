56eb4d1801a289df83e70c4837803dc3bdfdd853
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-03-17 17:13:37 +0200

    User Authentication

--- src/server/BSL/AuthBSl.java
+++ src/server/BSL/AuthBSl.java
@@ -4,23 +4,23 @@ import java.sql.*;
 
 import server.DB.DatabaseManager;
 import server.Models.Response;
+import server.Utils.Hashing;
 
 public class AuthBSl {
     private Connection connection;
 
     public Response signup(String name, String username, String password) {
         try {
-            this.connection = DatabaseManager.getConnection();
+            connection = DatabaseManager.getConnection();
+            password = Hashing.doHashing(password);
             String query = "INSERT INTO User (name, username, password) VALUES ('" +
                     name + "', '" + username + "', '" + password + "')";
 
-            // Create and execute statement
             Statement statement = connection.createStatement();
             int rowsInserted = statement.executeUpdate(query);
             Response response;
             if (rowsInserted > 0) {
                 response = new Response(200, "User registered successfully.");
-                // System.out.println("User registered successfully.");
             } else {
                 response = new Response(404, "Failed to register user.");
             }
@@ -31,11 +31,28 @@ public class AuthBSl {
         }
     }
 
-    public static void main(String[] args) {
-        DatabaseManager.connect();
-        AuthBSl authBSl = new AuthBSl();
-        Response response;
-        response = authBSl.signup("metwally", "msss101", "55555");
-        System.out.println(response.getStatus() + " " + response.getMessage());
+    public Response login(String username, String password) {
+        try {
+            connection = DatabaseManager.getConnection();
+            String query = "SELECT password FROM User WHERE username = '" + username + "'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            Response response;
+            if (resultSet.next()) {
+                String hashPassword = resultSet.getString("password");
+                if (!Hashing.comparePassword(password, hashPassword)) {
+                    response = new Response(401, "Unauthorized");
+                } else {
+                    response = new Response(200, "Welcome Back");
+                }
+            } else {
+                response = new Response(404, "User Not Found");
+            }
+    
+            return response;
+        } catch (Exception e) {
+            return new Response(500, e.toString());
+        }
     }
+
 }
