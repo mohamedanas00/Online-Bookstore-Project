0a75606ddbb0293705b00c0b43976446a9fb0c0a
mohamedanas00 <mohamed.anas7688@gmail.com>
2024-03-21 03:42:59 +0200

    user get operation

--- src/server/BSL/UserBSL.java
+++ src/server/BSL/UserBSL.java
@@ -1,64 +1,27 @@
 package server.BSL;
-
 import java.sql.*;
 
-import Models.GlobalResponse;
 import server.DB.DatabaseManager;
-import server.Utils.Authorization;
+import Models.User;
 
 public class UserBSL {
     private Connection connection;
     private Statement statement;
 
-    public GlobalResponse buyBook(int userId, int bookId) {
-        try {
-            connection = DatabaseManager.getConnection();
-            GlobalResponse response;
-            if (!Authorization.checkAuthorization(userId, "user", connection)) {
-                return new GlobalResponse(401, "Unauthorized");
-            }
-
-            // *Check the quantity of the book
-            String quantityQuery = "SELECT quantity FROM Book WHERE id = " + bookId;
-            statement = connection.createStatement();
-            ResultSet quantityResult = statement.executeQuery(quantityQuery);
-            if (quantityResult.next()) {
-                int quantity = quantityResult.getInt("quantity");
-                if (quantity > 0) {
-                    // *Decrement the quantity of the book
-                    String updateQuery = "UPDATE Book SET quantity = quantity - 1 WHERE id = " + bookId;
-                    int rowsUpdated = statement.executeUpdate(updateQuery);
-                    if (rowsUpdated > 0) {
-                        // *Insert into BookOwnership table
-                        String ownershipQuery = "INSERT INTO BookOwnership (user_id, book_id) VALUES ('" +
-                                userId + "', '" + bookId + "')";
-                        int rowsInserted = statement.executeUpdate(ownershipQuery);
-                        if (rowsInserted > 0) {
-                            response = new GlobalResponse(200, "Book bought successfully.");
-                        } else {
-                            response = new GlobalResponse(404, "Failed to insert into BookOwnership.");
-                        }
-                    } else {
-                        response = new GlobalResponse(404, "Failed to update book quantity.");
-                    }
-                } else {
-                    response = new GlobalResponse(404, "Book is out of stock.");
-                }
-            } else {
-                response = new GlobalResponse(404, "Book not found.");
-            }
-            statement.close();
-            return response;
-        } catch (Exception e) {
-            return new GlobalResponse(500, e.toString());
+    public User getUserDetails(int userId) throws SQLException{
+        connection =DatabaseManager.getConnection();
+        String query = "SELECT * FROM User WHERE id = '" + userId + "'";
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        User user;
+        if(resultSet.next()){
+            user= new User(resultSet);
+            user= new User(resultSet);
+            System.out.println(user);
+        }else{
+            return null;
         }
+
+        return user;   
     }
-    
-    // public static void main(String[] args) {
-    //     DatabaseManager.connect();
-    //     UserBSL userBSL =new UserBSL();
-    //     userBSL.buyBook(9, 5);
-
-    // }
-
 }
